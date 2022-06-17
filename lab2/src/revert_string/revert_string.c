cbc3e95d5f976dec19cd4cd9abe81dafc1f8b705
Romanov Artem <forward.afk@gmail.com>
2022-06-17 18:52:31 +0000

    Изменения в swap.c и revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include "string.h"
+#include "stdlib.h"
 
 void RevertString(char *str)
 {
-	// your code here
+        int str_len = strlen(str);
+        char* reverted = malloc((str_len + 1) * sizeof(char));
+        for (int i = 0, j = str_len - 1; i < str_len; i++, j--) {
+                reverted[i] = str[j];
+        }
+        reverted[str_len] = '\0';
+	strcpy(str, reverted);
 }
+
 
