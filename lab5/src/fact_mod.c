18ed0fd55d55fb184946891051f56bd39b345fda
Romanov Artem <forward.afk@gmail.com>
2022-06-21 02:11:45 +0000

    Факториал по модулю

--- lab5/src/fact_mod.c
+++ lab5/src/fact_mod.c
@@ -6,44 +6,51 @@
 #include <stdbool.h>
 #include <sys/time.h>
 
-size_t fact = 1;
-size_t p = 1;
+uint64_t fact = 1;
 
 pthread_mutex_t mutex_fact = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_p = PTHREAD_MUTEX_INITIALIZER;
 
 struct TFact {
-    size_t k;
-    size_t mod;
-    size_t begin;
-    size_t end;
+	uint64_t k;
+    uint64_t mod;
+    uint64_t begin;
+    uint64_t end;
 };
+
+uint64_t FactMod(uint64_t first, uint64_t sec, uint64_t mod) {
+	uint64_t result = 0;
+	first = first % mod;
+	while (sec > 0) {
+		if (sec % 2 == 1) {
+			result = (result + first) % mod;
+		}
+		first = (first * 2) % mod;
+		sec /= 2;
+	}
+  
+  return result % mod;
+}
 
 void *ThreadSum(void *args) {
     struct TFact *fargs = (struct TFact *)args;
-    size_t result = 1;
-    for (size_t i = fargs->begin; i <= fargs->end; i++) {
-        if (i%fargs->mod == 0) {
-            pthread_mutex_lock(&mutex_p);
-            result *= p;
-            p++;
-            pthread_mutex_unlock(&mutex_p);
-        } else {
-            result *= i%fargs->mod;
-        }
-		result %= fargs->mod;
-    }
+	uint64_t result = 1;
+	
+	for (int i = fargs->begin; i <= fargs->end; i++) {
+		result = FactMod(result, i, fargs->mod);
+	}
+	
     pthread_mutex_lock(&mutex_fact);
-    fact *= result % fargs->mod;
+    fact = FactMod(fact, result, fargs->mod);
     pthread_mutex_unlock(&mutex_fact);
     return 0;
 }
 
 int main(int argc, char **argv) {
 
-    uint32_t pnum = -1;
-    uint32_t mod = -1;
-    uint32_t k = -1;
+    uint64_t pnum = -1;
+    uint64_t mod = -1;
+    uint64_t k = -1;
 
     while (true) {
         static struct option options[] = {{"pnum", required_argument, 0, 0},
@@ -119,7 +126,7 @@ int main(int argc, char **argv) {
     struct timeval start_time;
     gettimeofday(&start_time, NULL);
 
-    for (uint32_t i = 0; i < pnum; i++) {
+    for (uint64_t i = 0; i < pnum; i++) {
         args[i].k = k;
         args[i].mod = mod;
 
@@ -141,6 +148,6 @@ int main(int argc, char **argv) {
     double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
     elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
-    printf("%d! mod %d = %ld, Time: %fms\n", k, mod, fact%mod, elapsed_time);
+    printf("%ld! mod %ld = %ld, Time: %fms\n", k, mod, fact, elapsed_time);
     return 0;
 }
